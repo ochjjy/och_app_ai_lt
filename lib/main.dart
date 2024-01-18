ad60a72c595944a63ed0d521ccac919921bba8ec
Michael Auk <och@kwic.co.kr>
2024-01-18 14:42:32 +0900

    add icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // remove after 3 seconds
+  Future.delayed(const Duration(seconds: 3), () {
+    FlutterNativeSplash.remove();
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
